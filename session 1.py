d94a885c125032d612593db1a9e9588ba07e95de
Aniketvs888 <41379947+Aniketvs888@users.noreply.github.com>
2022-07-17 20:02:01 +0530

    Add files via upload

---
+++ session 1.py
@@ -0,0 +1,343 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+print("Hrllo")
+
+
+# In[5]:
+
+
+#comment
+
+
+# In[8]:
+
+
+"""
+sdfvcxsxdcxz
+sdcvgrwda
+vadcx 
+"""
+print("Hello world")
+
+
+# In[10]:
+
+
+8*8*88*888*8
+
+
+# In[15]:
+
+
+16//3
+# quotient
+
+
+# In[14]:
+
+
+16%3
+#remainder
+
+
+# In[16]:
+
+
+3**2
+# power
+
+
+# In[17]:
+
+
+10**.2
+
+
+# In[18]:
+
+
+1.5848931924611136**5
+
+
+# In[4]:
+
+
+#strings
+str1 = "Aniket"
+str2 = "Satkar"
+print(st1)
+print(st2)
+
+
+# In[24]:
+
+
+x = 5
+
+
+# In[25]:
+
+
+x
+
+
+# In[26]:
+
+
+y = 5.2
+
+
+# In[27]:
+
+
+z = "HEllo"
+
+
+# In[29]:
+
+
+type(x)
+
+
+# In[30]:
+
+
+type(z)
+
+
+# In[31]:
+
+
+type(y)
+
+
+# In[32]:
+
+
+len(z)
+
+
+# In[6]:
+
+
+str = str1 + str2
+
+
+# In[7]:
+
+
+st
+
+
+# In[8]:
+
+
+str = str1 + " " +str2
+
+
+# In[9]:
+
+
+str
+
+
+# In[10]:
+
+
+len(str)
+
+
+# In[14]:
+
+
+#NEw line \n
+str3 = "How are you?"
+str4 = "HEllo, \n How are you?"
+
+print("HEllo, \n How are you?")
+
+
+# In[15]:
+
+
+#method
+
+str.capitalize()
+
+
+# In[17]:
+
+
+str5 = str.upper()
+str5
+
+
+# In[18]:
+
+
+#Casefold
+#compare
+str5==str
+
+
+# In[19]:
+
+
+str5.casefold()==str.casefold()
+
+#casefold makes the string of no case. So comparison becomes easy
+
+
+# In[20]:
+
+
+#find
+str1.find("a")
+
+
+# In[23]:
+
+
+#replace
+str1.replace("ket".casefold(),"keeeeet")
+
+
+# In[24]:
+
+
+#split
+str3.split()
+
+
+# In[25]:
+
+
+str3.split("are")
+
+
+# In[30]:
+
+
+#formating the output
+str6 = "Hello how are you {}".format("Aniket")
+
+
+# In[31]:
+
+
+str6
+
+
+# In[37]:
+
+
+print("Hello I am {} and I stay at {}".format("Aniket","Warora"))
+print("Hello I am {} and I stay at {}".format("Warora","Aniket"))
+print("Hello I am {1} and I stay at {0}".format("Warora","Aniket"))
+print("Hello I am {0} and I stay at {1}".format("Aniket","Warora"))
+
+print("Hello I am {name} and I stay at {place}".format(name = "Aniket",place = "Warora"))
+print("Hello I am {name} and I stay at {place}".format(place = "Warora", name = "Aniket"))
+
+
+# In[38]:
+
+
+
+
+
+# In[42]:
+
+
+#Indexing
+str = "Hello"
+len(str)    
+
+
+# In[41]:
+
+
+str[1] #index
+
+
+# In[44]:
+
+
+str[len(str)-1]
+
+
+# In[45]:
+
+
+#slicing
+#str[a:b]
+# a is included & b is excluded
+
+
+# In[51]:
+
+
+str = "Aniket"
+str[1:5]
+
+
+# In[49]:
+
+
+str[3:]
+
+
+# In[52]:
+
+
+str[::]
+
+
+# In[53]:
+
+
+str[:4]
+
+
+# In[54]:
+
+
+#reverse indexing
+str[-1]
+
+
+# In[56]:
+
+
+#reverse
+str[::-1]
+# -1 is step size 
+
+
+# In[57]:
+
+
+str[::-2]
+
+
+# In[58]:
+
+
+str[::2]    #forward 
+
+
+# In[59]:
+
+
+#strings can be muted with given methods only. we cannot assign it directly
+
+
+# In[ ]:
+
+
+
+
